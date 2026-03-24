e08ed9509caa2253a1adbafb8d49ca0ac4648950
Administrator <Administrator@A316-61>
2026-03-24 14:03:16 +0800

    updata

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Test
+{
+    private int Mid;
+    private int Final;
+
+    public int MyMid {get;} = 56;
+    public int MyFinal
+    {
+        get
+        {
+            return Final;
+        }
+        set
+        {
+            if ((value > 0) && (value < 100))
+            {
+                Final = value;
+            }
+            else
+            {
+                Final = 0;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Class_0324_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test std = new Test();
+            // std.MyMid = 92;
+            std.MyFinal = 78;
+            Console.WriteLine(std.MyMid.ToString());
+            Console.WriteLine(std.MyFinal.ToString());
+        }
+    }
+}
